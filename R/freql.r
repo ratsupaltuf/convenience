ce30245ad09302def06d0369cac6a7217eec9f2e
ratsupaltuf <72352171+ratsupaltuf@users.noreply.github.com>
2022-06-19 14:30:59 +0200

    changed options

--- R/freql.r
+++ R/freql.r
@@ -3,8 +3,10 @@
 #' @description This is a shorthand for freq(as_label(x))
 #'
 #' @param x A labelled vector
-#' @param y A title for the frequency table
+#' @param cap A character specifying the title for the frequency table.
+#' Defaul is the variable label
 #' @param kable Optional=should the output be processed by knitr::kable?
+#' @param d number of digits to be displayed
 #' @return Frequency table as produced by sjlabelled::freq of \code{x}, but with labels
 #' @examples
 #' freql(x, y="Some title")
@@ -13,7 +15,12 @@
 #' @importFrom sjlabelled get_label
 #' @export
 
-freql <- function(x, y="") {
-  summarytools::freq(as_label(x)) %>%
-    knitr::kable(caption=paste(y, "-", sjlabelled::get_label(x), sep=" "), digits=2)
+freql <- function(x, cap=sjlabelled::get_label(x), kable=T, d=3, ...) {
+  t<-summarytools::freq(as_label(x), round.digits = d)
+  if (kable==T & !is.null(cap)) {
+  knitr::kable(t, caption=cap)
+    }else if (kable==T)
+    {knitr::kable(t)
+      } else return(t)
+
 }
