01a320364d6b19eb33f0ac5f84bff1428730f3bf
ratsupaltuf <72352171+ratsupaltuf@users.noreply.github.com>
2023-05-04 18:10:39 +0200

    updated missing function

--- R/mis.r
+++ R/mis.r
@@ -1,20 +1,25 @@
-#' Calculate the percentage of missing values in a vector
+#' Calculate the percentage of missing values in a vector or a list of vectors
 #'
-#' This function takes a vector as input and calculates the percentage of missing values in it.
+#' This function calculates the percentage of missing values in a vector or a list of vectors.
 #'
-#' @param x A vector of numeric or character values
+#' @param x A vector or a list of vectors
 #'
-#' @return A numeric value representing the percentage of missing values in the input vector
+#' @return A numeric value or a vector of numeric values representing the percentage of missing values
 #'
 #' @examples
-#' mis(c(1, NA, 3, NA, 5))
-#' mis(c("a", "b", "", NA, "d"))
+#' mis(c(1,2,NA,4,NA)) # returns 40
+#' mis(list(c(1,2,NA), c(NA,3,4), c(NA,NA,NA))) # returns c(33.33, 66.67, 100)
 #'
 #' @export
 mis <- function(x) {
-  round(sum(is.na(x)) / length(x) * 100, 3)
+  if(is.list(x)) {
+    lapply(x, function(y) {
+      round(sum(is.na(y)) / length(y) * 100, 3)
+    })
+  } else {
+    round(sum(is.na(x)) / length(x) * 100, 3)
+  }
 }
-
 
 
 
