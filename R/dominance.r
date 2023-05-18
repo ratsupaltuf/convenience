fa98d89e573e8bbca61f8fbfdb43f46729abe7ac
ratsupaltuf <72352171+ratsupaltuf@users.noreply.github.com>
2023-05-18 15:38:03 +0200

    test2

--- R/dominance.r
+++ R/dominance.r
@@ -1,25 +1,26 @@
 #' Compute social class based on dominance principle
 #'
 #' This function assigns social class based on the dominance principle.
-#' The default assumes that lower values indicate higher class. The function takes two input variables, \code{resp} and \code{partner},
-#' which represent the respondent and partner, respectively. If the respondent's class is missing, it takes the partner's class.
-#' If either \code{resp} or \code{partner} is a factor variable, it is converted to numeric before computation. The resulting variable is
-#' assigned labels based on the labels of the \code{resp} variable.
+#' The function takes three input variables: \code{resp}, \code{partner}, and \code{higher}.
+#' \code{resp} and \code{partner} represent the respondent and partner, respectively. If the respondent's class is missing, it takes the partner's class.
+#' If either \code{resp} or \code{partner} is a factor variable, it is converted to numeric before computation.
+#' The resulting variable is assigned labels based on the labels of the \code{resp} variable.
+#' The \code{higher} parameter determines the interpretation of class values; if \code{higher} is FALSE (default), lower values indicate higher class. If \code{higher} is TRUE, higher values indicate higher class.
 #'
 #' @param resp A vector containing data for the respondent.
 #' @param partner A vector containing data for the partner.
+#' @param higher A logical value indicating whether higher values represent higher social class (default is FALSE).
 #'
 #' @return A vector containing the social class variable.
 #'
-#' @export
-#'
 #' @examples
-#' dominance(c(1,2,3), c(2,1,NA))
-#' dominance(factor(c("low","high","medium")), factor(c("high","low",NA)))
-#' dominance(c(1,2,3), c(2,1,NA)) %>% set_labels(c("Lower class", "Middle class", "Upper class"))
+#' dominance(c(1,2,3), c(2,1,NA), FALSE)
+#' dominance(factor(c("low","high","medium")), factor(c("high","low",NA)), TRUE)
+#' dominance(c(1,2,3), c(2,1,NA), FALSE) %>% set_labels(c("Lower class", "Middle class", "Upper class"))
 #'
 #' @importFrom sjlabelled get_labels as_numeric set_labels
 #' @importFrom dplyr case_when
+#' @export
 dominance<- function(resp, partner, higher=F) {
   # Assign social class based on dominance principle
   # if higher==F, lower values equal higher class
